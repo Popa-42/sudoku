c6b5d0989102e99fb52dc52f688e6f27e9046611
Popa <115492561+Popa-42@users.noreply.github.com>
2025-08-25 15:57:35 +0200

    Update better-tailwindcss ESLint plugin configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -47,32 +47,6 @@ const eslintConfig = [
           project: "./tsconfig.json",
         },
       },
-      "better-tailwindcss": {
-        attributes: ["className", "classNames", "classes", "styles", "style", ".*Classes", ".*Variants"],
-        callees: ["clsx", "cn", "cva", "tw", ".*Classes", ".*Variants"],
-        variables: [
-          ".*Classes",
-          ".*Variants",
-          [
-            ".*Classes",
-            [
-              {
-                match: "objectValues",
-                pathPattern: "[a-z]+[A-Za-z]*",
-              },
-            ],
-          ],
-          [
-            ".*Variants",
-            [
-              {
-                match: "objectValues",
-                pathPattern: "[a-z]+[A-Za-z]*",
-              },
-            ],
-          ],
-        ],
-      },
     },
   }),
   {
@@ -109,6 +83,8 @@ const eslintConfig = [
           group: "newLine",
         },
       ],
+      "better-tailwindcss/enforce-consistent-class-order": "warn",
+      "better-tailwindcss/enforce-consistent-variable-syntax": "warn",
       "better-tailwindcss/no-unregistered-classes": ["off"],
 
       // Prettier
@@ -124,6 +100,30 @@ const eslintConfig = [
       "better-tailwindcss": {
         // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
         entryPoint: "public/assets/styles/globals.css",
+
+        attributes: ["class", "className", ".*Class(Name|es)?$", ".*Variants?$"],
+        callees: [
+          "cva",
+          "cx",
+          "cn",
+          "clsx",
+          "tw",
+          "tv",
+          ".*Variants?$",
+          [
+            "cva",
+            [
+              {
+                match: "strings",
+              },
+              {
+                match: "objectValues",
+                pathPattern: "^variants(\\.[a-z]+[a-zA-Z]*)+$",
+              },
+            ],
+          ],
+        ],
+        variables: [".*Variants?$", ".*Classes?$"],
       },
     },
   },
